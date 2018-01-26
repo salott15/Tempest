910c4b28e002b14aa39e386c3c5179fd7d91087e
salott15 <sarilott@yahoo.com>
2018-01-25 18:20:28 -0800

    test

--- public/app.js
+++ public/app.js
@@ -1,18 +1,22 @@
 $('document').ready(function(){
 
-	$("#empty").hover(function(){
-	$(this).find("span").html("Empty");
+    $("#empty").hover(function(){
+    $(this).find("span").html("Empty");
 },
 function(){
-	$(this).find("span").html("");
+    $(this).find("span").html("");
+})
+
+$('#info').on('click', function() {
+    $('#instructions').show()
 })
 //Home Page
-$('#instructions .close').click(function(){ $('#instructions').remove(); });
-	$("#full").hover(function(){
-	$(this).find("span").html("Full");
+$('#instructions .close').click(function(){ $('#instructions').hide(); });
+    $("#full").hover(function(){
+    $(this).find("span").html("Full");
 },
 function(){
-	$(this).find("span").html("");
+    $(this).find("span").html("");
 })
 let fullHref = `/bar/status/full/${localStorage.getItem("boundsLatOne")}/${localStorage.getItem("boundsLat2")}/${localStorage.getItem("boundsLngOne")}/${localStorage.getItem("boundsLng2")}`
 $("#full").closest("a").attr("href", fullHref)
@@ -21,45 +25,45 @@ $("#empty").closest("a").attr("href", emptyHref)
 
 //Bar Page
 $(".voteFull, .voteEmpty").click(function() {
-	console.log($(this).attr("data-barId"));
-	$.ajax({url:"/bar/current", method: "put", data:{
-		barname: $(this).attr("data-barname"),
-		barId: $(this).attr("data-barId"),
-		busy: ($(this).hasClass("voteFull")) ? "full":"empty",
-		lat: $(this).attr("data-lat"),
-		lng: $(this).attr("data-lng")}})
-	.done(function(data){
-		console.log("done", data)
-	});
+    console.log($(this).attr("data-barId"));
+    $.ajax({url:"/bar/current", method: "put", data:{
+        barname: $(this).attr("data-barname"),
+        barId: $(this).attr("data-barId"),
+        busy: ($(this).hasClass("voteFull")) ? "full":"empty",
+        lat: $(this).attr("data-lat"),
+        lng: $(this).attr("data-lng")}})
+    .done(function(data){
+        console.log("done", data)
+    });
 });
 
 $(".saveBar").click(function(){
-	if(localStorage.getItem("userName")){
-		$.ajax({
-			url:"/user/current",
-			method: "post",
-			data:{
-				barid: $(this).attr("data-barid"),
-				uid: localStorage.getItem("uid"),
-			},
-			success: function(response) {
-				window.location.href= "/user/mybars"
-			}
-		})
-	}
-	else{
-		window.location.href= "/userlogin"
-	}
+    if(localStorage.getItem("userName")){
+        $.ajax({
+            url:"/user/current",
+            method: "post",
+            data:{
+                barid: $(this).attr("data-barid"),
+                uid: localStorage.getItem("uid"),
+            },
+            success: function(response) {
+                window.location.href= "/user/mybars"
+            }
+        })
+    }
+    else{
+        window.location.href= "/userlogin"
+    }
 });
 
 $(".deleteBar").click(function(){
-	$.ajax({
-		url:"/user/mybars",
-		method: "delete",
-		data:{ barid: $(this).attr("data-id")},
-		success: function(response) {
-			window.location.reload()
-		}
-	})
+    $.ajax({
+        url:"/user/mybars",
+        method: "delete",
+        data:{ barid: $(this).attr("data-id")},
+        success: function(response) {
+            window.location.reload()
+        }
+    })
 })
 });
